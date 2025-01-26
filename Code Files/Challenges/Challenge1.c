abfa4f9a214f30e3ab179bb93ff89084ebefc64c
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-26 16:54:27 +1300

    completed first challenge

---
+++ Code Files/Challenges/Challenge1.c
@@ -0,0 +1,22 @@
+/*
+Write a program that initializes the following variables:
+
+k of type int and value 180
+half of type float and value 0.5
+a of type char and value 'c'
+The variables can be initialized in any order.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // Write code here
+    int k = 180;
+    float half = 0.5;
+    char a = 'c';
+
+    // Don't change below this line
+    printf("k = %d, half = %2.1f, a = %c", k, half, a);
+    return 0;
+};
