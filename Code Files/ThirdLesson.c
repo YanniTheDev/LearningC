f2a1da8080fd586e08ac86793becd59cf19f29a6
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-27 19:10:56 +1300

    lovely learning about input in C

---
+++ Code Files/ThirdLesson.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // To get input from user
+
+    int x;
+    scanf("%d", &x);
+
+    // Ampersand means that it will return the address of the variable x.
+    // It will input the value of what the user inputs into the memory address (which is the variable: x)
+}
